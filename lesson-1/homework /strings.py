771e9d084fb235936a293c74e0350b798abd5dd3
AlisherSQL <atadjanovalisher07@gmail.com>
2025-04-29 18:00:29 +0500

    Create strings.py

---
+++ lesson-1/homework /strings.py
@@ -0,0 +1,24 @@
+Given a side of square. Find its perimeter and area.
+side1=int(input())
+perimeter=4*side1
+area=side1**2
+print (f'Perimeter:{perimeter}')
+print (f'Area:{area}')
+
+Given diameter of circle. Find its length.
+side=int(input())
+circul=side*3.14
+print(f'Len of Circul:{circul}')
+
+Given two numbers a and b. Find their mean.
+a = int(input())
+b = int(input())
+print(f'javob{a*b/2}')
+
+Given two numbers a and b. Find their sum, product and square of each number.
+a = int(input())
+b = int(input())
+print (f'Sum:{a+b}')
+print (f'Product:{a*b}')
+print (f'Square:{a**2}')
+print (f'Square:{b**2}')
