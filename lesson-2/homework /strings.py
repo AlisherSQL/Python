47812ca67852605e541ca6480ebb5787d37b1d83
AlisherSQL <atadjanovalisher07@gmail.com>
2025-04-29 18:13:45 +0500

    Create strings.py

---
+++ lesson-2/homework /strings.py
@@ -0,0 +1,43 @@
+1.Write a Python program to ask for a user's name and year of birth, then calculate and display their age.
+from datetime import datetime
+name=input()
+year_of_birth= int(input())
+current_year = datetime.now().year
+age=current_year-year_of_birth
+print(f'Hello, {name}! You are {age} years old.')
+
+2.Extract car names from the following text:
+txt='LMaasleitbtui'
+txt[1::2]
+txt[::2]
+
+3.Extract car names from the following text:
+txt1 = 'MsaatmiazD'
+txt1[::2]
+txt1[1::2]
+
+4.Extract the residence area from the following text:
+words = txt3.split()
+residence_area = words[5] 
+print(residence_area)
+
+5.Write a Python program that takes a user input string and prints it in reverse order.
+name='Hello World'
+name[::-1]
+
+6.Write a Python program that counts the number of vowels in a given string.
+
+7.Write a Python program that takes a list of numbers as input and prints the maximum value.
+numberMax=int(input())
+numberMax.count(numberMax)
+print(f'MaxNumber{numberMax}')
+
+8.Write a Python program that checks if a given word is a palindrome (reads the same forward and backward).
+
+
+  
+
+
+  
+
+
