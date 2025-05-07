43486211c717f2bcee9f549633b04f4fe003c803
AlisherSQL <atadjanovalisher07@gmail.com>
2025-05-07 14:21:42 +0500

    Create strings.py

---
+++ lesson-3/Homework/strings.py
@@ -0,0 +1,98 @@
+1.Create a list containing five different fruits and print the third fruit.
+fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"]
+print(fruits[2])
+
+2.Create two lists of numbers and concatenate them into a single list.
+l1=[1,2,3]
+l2=[2,3,4]
+Comb=l1+l2
+print(Comb)
+
+3. Given a list of numbers, extract the first, middle, and last elements and store them in a new list.
+name=[1,2,3,4,5,6,7,8]
+first=name[0]
+middle=name[len(name)//2]
+last=name[-1]
+AAA=[first,middle,last]
+print(AAA)
+
+4.Create a list of your five favorite movies and convert it into a tuple.
+S= ['Ass','Dfff','Fggg']
+A= tuple(S)
+print(A)
+
+5.Given a list of cities, check if "Paris" is in the list and print the result.
+city=['London','Paris','China']
+if  'Paris' in city:
+    print('Paris')
+else:
+    print('No')
+
+6.Create a list of numbers and duplicate it without using loops.
+a=[1,2,3,45]
+asd=a+a
+print(asd)
+
+7. Given a list of numbers, swap the first and last elements.
+a=[1,2,3,4,55]
+a.reverse()
+ print(a)
+
+8. Create a tuple of numbers from 1 to 10 and print a slice from index 3 to 7.
+a=(1,2,3,4,5,6,7,8,9,10)
+print(a[3:7])
+
+9.Create a list of colors and count how many times "blue" appears in the list.
+a=['red','blue','green','blue']
+print(a.count('blue'))
+
+10. Given a tuple of animals, find the index of "lion".
+a=('cat','dog','lion')
+print(a.index('lion'))
+
+11.Create two tuples of numbers and merge them into a single tuple.
+a=(1,2,3,4)
+b=(3,4,5)
+print(a+b)
+
+12.Given a list and a tuple, find and print their lengths.
+a=[1,2,3,4,5]
+print(len(a))
+
+13.Create a tuple of five numbers and convert it into a list.
+a=[1,2,3]
+print(tuple(a))
+
+14. Given a tuple of numbers, find and print the maximum and minimum values.
+a=(1,2,3,4,5,6,7)
+print([max(a) , min(a)])
+
+15.Create a tuple of words and print it in reverse order.
+a=('Hello','world')
+print(a[::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
